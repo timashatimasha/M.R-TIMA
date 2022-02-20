73bf52ad906b33817f7f17ac7f80ebdc67985cdc
timashatimasha <98791752+timashatimasha@users.noreply.github.com>
2022-02-20 04:40:35 -0800

    Update and rename TrexTeam.js to AlexaTeam.js

---
+++ AlexaTeam.js
@@ -0,0 +1,5 @@
+module.exports = {
+    ALEXA_OWNER: '94715264791', // Alexa Team - QUEEN ALEXA Owner
+    ALEXA_TEAM: '94773585511,94764820230,94741348404,9471 512 2309', // Team - QUEEN ALEXA TEAM
+    ALEXA_OWNER: '94715264791' // Timasha -  Owner --> For The Friendship And co Owner in Alexa
+};
